f60898b0f8846b1725dfee38ec6bd4334aad59b3
Peter Klimenko <megalance@gmail.com>
2019-09-03 08:11:38 +0400

    Fix parse

--- app/Console/Commands/ParseData.php
+++ app/Console/Commands/ParseData.php
@@ -9,53 +9,58 @@ use App\Http\Controllers\Parser\FreelansimController;
 use App\Http\Controllers\Parser\GuruController;
 use App\Http\Controllers\Parser\WeblancerController;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
-class ParseData extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'parse:data';
+class ParseData extends Command {
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'parse:data';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Command description';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $freelanceHunt = new FreelanceHuntController();
-        $weblancer = new WeblancerController();
-        $fl = new FlController();
-        $freelanceRu = new FreelanceRuController();
-        $freelansim = new FreelansimController();
-        $guru = new GuruController();
-        $freelanceHunt->fillData();
-        $weblancer->fillData();
-        $fl->fillData();
-        $freelanceRu->fillData();
-        $freelansim->fillData();
-        $guru->fillData();
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
 
-        return true;
-    }
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle() {
+		try {
+			$freelanceHunt = new FreelanceHuntController();
+			$weblancer     = new WeblancerController();
+			$fl            = new FlController();
+			$freelanceRu   = new FreelanceRuController();
+			$freelansim    = new FreelansimController();
+			$guru          = new GuruController();
+			$freelanceHunt->fillData();
+			$weblancer->fillData();
+			$fl->fillData();
+			$freelanceRu->fillData();
+			$freelansim->fillData();
+			$guru->fillData();
+		} catch (\Exception $e) {
+
+			Log::debug($e->getMessage());
+
+		}
+
+		return true;
+	}
 }
