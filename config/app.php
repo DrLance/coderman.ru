fbb22a614c82eae31e1b389e829ce178208c069b
Peter Klimenko <megalance@gmail.com>
2019-10-12 10:30:08 +0400

    Add new mon system

--- config/app.php
+++ config/app.php
@@ -162,10 +162,10 @@ return [
     Illuminate\Translation\TranslationServiceProvider::class,
     Illuminate\Validation\ValidationServiceProvider::class,
     Illuminate\View\ViewServiceProvider::class,
-
     /*
      * Package Service Providers...
      */
+    Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
 
     /*
      * Application Service Providers...
@@ -227,6 +227,7 @@ return [
     'URL'          => Illuminate\Support\Facades\URL::class,
     'Validator'    => Illuminate\Support\Facades\Validator::class,
     'View'         => Illuminate\Support\Facades\View::class,
+    'Bugsnag' => Bugsnag\BugsnagLaravel\Facades\Bugsnag::class,
 
   ],
 
