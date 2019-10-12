7e4af4d358f615292539e53e94c65f678c2b5609
Peter Klimenko <megalance@gmail.com>
2019-10-12 10:51:02 +0400

    Fix!!!!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,57 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Pages\StatController;
-use App\Http\Controllers\Parser\PchelController;
-use App\Http\Controllers\Parser\UpworkController;
 use App\Models\Type;
 use Backpack\PageManager\app\Models\Page;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App;
 
-class HomeController extends Controller {
+class HomeController extends Controller
+{
 
-	public $data;
+    public $data;
 
-	public function index(Request $request) {
+    public function index(Request $request)
+    {
 
-		$types = Type::all();
+        $types = Type::all();
 
-		$this->data['types'] = $types;
+        $this->data['types'] = $types;
 
-		$page = Page::findBySlug('home');
+        $page = Page::findBySlug('home');
 
-		if($page) {
-			$this->data['page'] = $page->withFakes();
-		}
+        if ($page) {
+            $this->data['page'] = $page->withFakes();
+        }
 
-		return view('welcome', $this->data);
-	}
+        return view('welcome', $this->data);
+    }
 
-	public function about(Request $request) {
-		$page = Page::findBySlug('about');
+    public function about(Request $request)
+    {
+        $page = Page::findBySlug('about');
 
-		if ( ! $page) {
-			abort(404);
-		}
+        if ( ! $page) {
+            abort(404);
+        }
 
-		return view('pages.about_us', ['page' => $page->withFakes()]);
-	}
+        return view('pages.about_us', ['page' => $page->withFakes()]);
+    }
 
-	public function lang($locale)
-	{
-		App::setLocale($locale);
-		session()->put('locale', $locale);
-		return redirect()->back();
-	}
+    public function lang($locale)
+    {
+        App::setLocale($locale);
+        session()->put('locale', $locale);
+
+        return redirect()->back();
+    }
 
 
-	public function test(Request $request) {
+    public function test(Request $request)
+    {
 
-				$fl = new UpworkController();
-				$fl->fillData();
-
-	}
+    }
 
 }
